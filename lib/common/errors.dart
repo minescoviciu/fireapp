86aa2935d67fde3884ba9a8956f66a65622f3ae4
Andrei Minescu <minescoviciu@192-168-0-102.rdsnet.ro>
2022-07-23 19:50:44 +0300

    Common errors

---
+++ lib/common/errors.dart
@@ -0,0 +1,5 @@
+class FireAppErrors {
+  static const String passwordToShort = 'Password is to short';
+  static const String emailIsRequired = 'Email is required';
+  static const String emailNotAnEmail = 'Enter a valid email';
+}
